3b869be228a1bbe23eb1766a68c986545bfb7be2
esrasnck <esrasancak@hotmail.com>
2021-02-05 15:58:33 +0300

    Alt Yapı Hazırlandı

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,45 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T> where T:class, IEntity, new()
+
+    {
+        /// <summary>
+        /// Veri kaynağından aldığımız tüm entity listeler
+        /// </summary>
+        /// <returns> listesi döndürür.</returns>
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+
+        /// <summary>
+        /// Veri kaynağına entity ekler
+        /// </summary>
+        /// <param name="entity">Eklenecek entity</param>
+        void Add(T entity);
+
+        /// <summary>
+        /// Veri kaynağınadaki entity'i Günceller.
+        /// </summary>
+        /// <param name="entity">Güncel Entity bilgileri</param>
+        void Update(T entity);
+
+        /// <summary>
+        /// Veri kaynağınan entityi Siler
+        /// </summary>
+        /// <param name="entity">Silinecek entity</param>
+        void Delete(T entity);
+
+        /// <summary>  // todo: sonradan bak.
+        /// Veri kaynağından, entity'i şarta göre getirir.
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns>Bir tek bir entity döndürür.</returns>
+        T Get(Expression<Func<T, bool>> filter);
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfColorDal.cs
@@ -0,0 +1,73 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfColorDal : IColorDal
+    {
+        public void Add(Color entity)
+        {
+            using (RentACarDBContext context = new RentACarDBContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+
+            };
+        }
+
+        public void Delete(Color entity)
+        {
+            using (RentACarDBContext context = new RentACarDBContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+
+            };
+        }
+
+        public Color Get(Expression<Func<Color, bool>> filter)
+        {
+            using (RentACarDBContext context = new RentACarDBContext())
+            {
+                return context.Set<Color>().SingleOrDefault(filter);
+
+            };
+        }
+
+        public List<Color> GetAll(Expression<Func<Color, bool>> filter = null)
+        {
+            using (RentACarDBContext context = new RentACarDBContext())
+            {
+                if (filter == null)
+                {
+                    return context.Set<Color>().ToList();
+                }
+                else
+                {
+                    return context.Set<Color>().Where(filter).ToList();
+                }
+
+            };
+        }
+
+        public void Update(Color entity)
+        {
+            using (RentACarDBContext context = new RentACarDBContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+
+            };
+        }
+    }
+}
